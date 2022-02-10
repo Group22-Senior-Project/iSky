a7044bb81c6703d106ce3736ecf7235e7ae74494
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-02-09 21:21:08 -0800

    added header

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/Header.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
