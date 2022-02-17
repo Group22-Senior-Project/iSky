0cf432feb18a884797af1c90745a147675e94be4
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-02-17 10:28:39 -0800

    added footer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/Header.js';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Header />
+    <Footer />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
