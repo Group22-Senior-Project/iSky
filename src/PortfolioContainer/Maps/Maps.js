914d94c2b2f1c0de47a079e42749a8fb0e838ae2
Richard <richardtran298@gmail.com>
2022-04-14 11:37:03 -0700

    updated parent weather and maps

--- src/PortfolioContainer/Maps/Maps.js
+++ src/PortfolioContainer/Maps/Maps.js
@@ -2,8 +2,9 @@ import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../utilities/ScrollService';
 import Animations from '../../utilities/Animations';
 import './Maps.css';
-import React from 'react'
+import { React, useState } from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+// import { useState } from 'react/cjs/react.production.min';
 
 export default function Maps(props) {
    // Scrolling Animation
@@ -15,18 +16,29 @@ export default function Maps(props) {
    // const fadeInSubscription =
       ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-const containerStyle = {
-   width: '100%',
-   height: '600px'
+   
+   // const [coord, setCoord] = useState(props.coord);
+   // console.log(props);
+   const coord = {
+      lat: props.lat,
+      lng: props.lon
+   };
+   // console.log(coord);
+   
+   
+
+   const containerStyle = {
+      width: '100%',
+      height: '600px'
+   
+   };
  
- };
- 
- const location = {
-   lat: 37.3382, 
-   lng: -121.8863
-//  lat: 33.610600, 
-//  lng: -112.149640
- };
+   // const location = {
+   //    lat: 37.3382, 
+   //    lng: -121.8863
+   // //  lat: 33.610600, 
+   // //  lng: -112.149640
+   // };
  
 
    const { isLoaded } = useJsApiLoader({
@@ -44,7 +56,7 @@ const containerStyle = {
                id="circle-example"
                mapContainerStyle={containerStyle}
                zoom={14}
-               center={location}
+               center={coord}
             />
                { /* Child components, such as markers, info windows, etc. */ }
             
--- src/PortfolioContainer/PortfolioContainer.js
+++ src/PortfolioContainer/PortfolioContainer.js
@@ -1,21 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TOTAL_SCREENS } from '../utilities/commonUtils';
+import axios from 'axios';
 
 export default function PortfolioContainer() {
-   const mapAllScreens = () => {
-      return TOTAL_SCREENS.map((screen) =>
-         screen.component ? (
-            <screen.component
-               screenName={screen.screen_name}
-               key={screen.screen_name}
-               id={screen.screen_name}
-            />
-         ) : (
-            <div key={screen.screen_name}></div>
-         )
-      );
+   const Home = TOTAL_SCREENS[0];
+   const Map = TOTAL_SCREENS[1];
+   const Weather = TOTAL_SCREENS[2];
+   const Corona = TOTAL_SCREENS[3];
+
+   /* OpenWeatherMap API */
+   const [data, setData] = useState({});
+   const [location, setLocation] = useState('');
+
+   // sets variables and the update functions
+   // for longitude and latitude 
+   // default variables are within useState parentheses
+   const [lat, setLat] = useState(37.3382)
+   const [lon, setLon] = useState(-121.8863)
+
+   // const [coord, setCoord] = useState({
+   //    lat: 37.3382, 
+   //    lng: -121.8863
+   // });
+
+   // OpenWeatherMap API Key by city name
+   // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+   const key = '7f58ed63d7854545d442c43cba9d26af';
+   let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${key}`;
+
+   const searchLocation = (event) => {
+      if (event.key === 'Enter') {
+         axios.get(url).then((response) => {
+            setData(response.data);
+            console.log(response.data);
+            // setCoord(response.data.coord)
+            // console.log(response.data.coord)
+            setLat(response.data.coord.lat)
+            setLon(response.data.coord.lon)
+         });
+         // Resets the search text to ''
+         setLocation('');
+      }
    };
 
-   return <div className="portfolio-container">{mapAllScreens()} </div>
-   
+
+   return (<div>
+      <Home.component
+               screenName={Home.screen_name}
+               key={Home.screen_name}
+               id={Home.screen_name}
+            />
+      <div className="search">
+        <input 
+          type="text" 
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+        />
+      </div>
+      <Map.component
+               screenName={Map.screen_name}
+               key={Map.screen_name}
+               id={Map.screen_name}
+               // coord={coord}
+               lat={lat}
+               lon={lon}
+            />
+      <Weather.component
+               screenName={Weather.screen_name}
+               key={Weather.screen_name}
+               id={Weather.screen_name}
+               data={data}
+            />
+      <Corona.component
+               screenName={Corona.screen_name}
+               key={Corona.screen_name}
+               id={Corona.screen_name}
+            />
+   </div>)
 }
